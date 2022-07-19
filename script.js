c9fb2035204340ee9d52634b3084b2e651060f61
Jack Smallwood <jacksmallwood@icloud.com>
2022-07-19 17:06:38 +0100

    Added logic for random index of characters & function to write password

--- script.js
+++ script.js
@@ -3,92 +3,60 @@ let generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
 // Array elements
-let all = "";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*_-+=";
-
-// function getRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-// }
-
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-// }
-
-// function getRandomNumber() {
-//   return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-// }
-
-// function getRandomSymbol() {
-//   var symbols = "!@#$%^&*_-+=";
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-// var characters = {
-//   lower: getRandomLower,
-//   upper: getRandomUpper,
-//   numbers: getRandomNumber,
-//   symbols: getRandomSymbol,
-// };
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!#$%^&*_-+=";
 
 // Alerts and Password Character Selection
 function generatePassword() {
-  all = "";
+  let all = "";
   let passwordLength = prompt(
     "Password Criteria = Minimum of 8 characters, Maximum of 128 characters"
   );
-  if (
-    passwordLength < 8 ||
-    passwordLength > 128 
-    //not sure here? ^^^^^
-  ) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert(
       "Does not meet criteria - Please enter a value between 8-128 characters"
     );
+    generatePassword();
   } else {
-    let includeLower = confirm("Include lower case characters?");
+    const includeLower = confirm("Include lower case characters?");
     if (includeLower) {
       all += lower;
     }
-    let includeUpper = confirm("Include upper case characters?");
+    const includeUpper = confirm("Include upper case characters?");
     if (includeUpper) {
       all += upper;
     }
-    let includeNumbers = confirm("Include numbers?");
+    const includeNumbers = confirm("Include numbers?");
     if (includeNumbers) {
       all += numbers;
     }
-    let includeSymbols = confirm("Include symbols & special characters?");
+    const includeSymbols = confirm("Include symbols & special characters?");
     if (includeSymbols) {
       all += symbols;
     }
 
-    if (
-      includeLower === false &&
-      includeUpper === false &&
-      includeNumbers === false &&
-      includeSymbols === false
-    ) {
+    if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
       alert("Must select one character type to continue");
       generatePassword();
     }
   }
   let password = "";
 
-  for (let i = 0; i < passwordLength; i++) {
-    password += everyCharacter.charAt(
-      Math.floor(Math.random() * everyCharacter.length)
-    );
-  }
+  console.log(all);
 
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * all.length);
+
+    password += all[randomIndex];
+  }
+  console.log(password);
   return password;
 }
 
 // Get references to the #generate element
-// var generateBtn = document.getElementById("generate");
-var passwordText = document.getElementById("password"); // result
+var passwordText = document.getElementById("password");
 var passwordLength = document.getElementById("length");
 var includeNumbers = document.getElementById("numbers");
 var includeSymbols = document.getElementById("symbols");
@@ -98,12 +66,9 @@ var includeLower = document.getElementById("lower");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
-  document.getElementById("passwordText").value = password;
+  console.log(password);
+  document.getElementById("password").value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-{
-  passwordText.value = writePassword(length.value, characters);
-}
